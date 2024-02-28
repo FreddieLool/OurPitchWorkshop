319fc20f1bcd3bfcdd1c057936c47380f71662e1
Ellie2405 <mttne2405@gmail.com>
2024-02-28 17:38:29 +0200

    touch panel

--- Assets/TouchScreen.cs
+++ Assets/TouchScreen.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class TouchScreen : MonoBehaviour
 {
@@ -9,10 +11,20 @@ public class TouchScreen : MonoBehaviour
 
     [SerializeField] Transform Ink;
     [SerializeField] int poolSize;
+    [SerializeField] GameObject TouchScreenObject;
+    [SerializeField] GameObject ProjectScreenObject;
     Transform[] drawingPool;
+
+    Vector3 screenHeight;
+    bool draw = false;
+    private PointerEventData m_PointerEventData;
+    [SerializeField] private EventSystem m_EventSystem;
+    [SerializeField] private GraphicRaycaster m_Raycaster;
 
     private void Start()
     {
+        screenHeight = new(0, Screen.height / 2, 0);
+
         drawingPool = new Transform[poolSize];
         for (int i = 0; i < poolSize; i++)
         {
@@ -21,20 +33,33 @@ public class TouchScreen : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            draw = true;
+        }
+        else draw = false;
+    }
 
     private void FixedUpdate()
     {
-        if (Input.GetMouseButton(0))
+        if (draw)
         {
             //drawingPool[0].position = GetPosition();
             //Transform a = Instantiate(Ink);
             Transform a = GetPoolObject();
             if (a != null)
             {
-                a.position = GetPosition();
+                a.position = ProjectToScreen(BadGetMousePosition());
 
             }
         }
+    }
+
+    Vector2 ProjectToScreen(Vector2 v)
+    {
+        return new(v.x, v.y);
     }
 
     Transform GetPoolObject()
@@ -52,8 +77,28 @@ public class TouchScreen : MonoBehaviour
         return null;
     }
 
-    private Vector2 GetPosition()
+    private Vector2 GetMousePosition()
     {
+        //Set up the new Pointer Event
+        m_PointerEventData = new PointerEventData(m_EventSystem);
+        //Set the Pointer Event Position to that of the game object
+        m_PointerEventData.position = Input.mousePosition;
+
+        //Create a list of Raycast Results
+        List<RaycastResult> results = new List<RaycastResult>();
+
+        //Raycast using the Graphics Raycaster and mouse click position
+        m_Raycaster.Raycast(m_PointerEventData, results);
+
+        if (results.Count > 0)
+        {
+            //Debug.Log("Hit " + results[0].gameObject.name);
+        }
         return _camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private Vector2 BadGetMousePosition()
+    {
+        return _camera.ScreenToWorldPoint(Input.mousePosition + screenHeight);
     }
 }
